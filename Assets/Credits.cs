ed4cb6d4ac28d3a443eeaaca1bedccf3be305749
bautinogal <jbnogal@gmail.com>
2020-03-20 04:24:13 -0300

    Menu principal con créditos

---
+++ Assets/Credits.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Credits : MonoBehaviour
+{
+    [SerializeField]
+    Button salir;
+
+    private void Start()
+    {
+        salir.onClick.AddListener(() => SceneManager.LoadScene(0));
+    }
+}
---
+++ Assets/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField]
+    Button comenzar;
+    [SerializeField]
+    Button creditos;
+    [SerializeField]
+    Button salir;
+
+    private void Start()
+    {
+        comenzar.onClick.AddListener(() => SceneManager.LoadScene(2));
+        creditos.onClick.AddListener(() => SceneManager.LoadScene(1));
+        salir.onClick.AddListener(() => Application.Quit());
+    }
+}
